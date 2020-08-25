8a254a0c0a34a3582f194dd811f3cacfbe3ee7d0
kangdain <juhyueon3@naver.com>
2020-08-26 00:59:11 +0900

    Learning redux modifier,store,dispatch

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
-import { createStore } from  "redux";
+import { createStore } from "redux";
 
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const reducer = (count = 0 ) => {
-  return count;
-} // reducer ? 1. modify your date 2. whatever it returns  that becomes the data on your application. 
+number.innerText = 0;
 
-const countStore = createStore(reducer); // store ? put your data
-console.log(countStore.getState());
+const ADD = "ADD";
+const MINUS = "MINUS";
 
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
 
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
