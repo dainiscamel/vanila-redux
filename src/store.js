a2d8eba753d0eb2b43868beb550c22d54fbaecd0
kangdain <juhyueon3@naver.com>
2020-09-03 00:03:46 +0900

    redux toolkit

--- src/store.js
+++ src/store.js
@@ -1,38 +1,16 @@
-import { createStore } from "redux";
-import { createAction } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) => state.filter(toDo => toDo.id !== action.payload)
+  }
+});
 
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     case addToDo.type:
-//       return [{ text: action.payload, id: Date.now() }, ...state];
-//     case deleteToDo.type:
-//       return state.filter(toDo => toDo.id !== action.payload);
-//     default:
-//       return state;
-//   }
-// };
+export const { add, remove } = toDos.actions;
 
-
-
-const reducer = createReducer([], {
-  [addToDo] : (state, action) => {
-    // 1.createReducer를 사용하면 state 를 mutate 하기에 용이. (= return 하지 않음.)
-    state.push({text: action.payload, id: Date.now()})
-  },
-  [delelteToDo] : (state, action) => {
-    // 2. createReducer를 사용하면 state를 return 할 수 있다.(= mutate 하지 않음/)
-    state.filter(toDo => toDo.id !== action.payload);
-  },
-})
-
-const store = createStore(reducer);
-
-export const actionCreators = {
-  addToDo,
-  deleteToDo
-};
-
-export default store;
+export default configureStore({ reducer: toDos.reducer });
